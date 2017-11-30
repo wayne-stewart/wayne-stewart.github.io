e7f55fbbd6b7a957a081381914be82069e5f8ea4
Wayne Stewart <waynegaidin@hotmail.com>
2017-11-29 21:11:32 -0600

    Update app.js

--- sleeping-crane/app.js
+++ sleeping-crane/app.js
@@ -1,3 +1,13 @@
 (function() {
-  alert('sleeping crane');
+  let iframe = document.createElement("iframe");
+  document.body.appendChild(iframe);
+  let worker_canvas = iframe.contentDocument.createElement("canvas");
+  let worker_ctx = worker_canvas.getContext("2d");
+  
+  var divView = document.getElementsByClassName("view")[0];
+  for(var i = 0; i < divView.childNodes.length; i++) { 
+    console.log(divView.childNodes[i]);
+  }
+  
+  document.body.removeChild(iframe);
 })();
