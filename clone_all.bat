44db3b54ec5b20a2ecf1a9a1a1824577acb23fc5
Wayne Stewart <waynegaidin@hotmail.com>
2021-05-26 19:28:41 -0500

    added clone_all.bat

---
+++ clone_all.bat
@@ -0,0 +1,25 @@
+
+git clone https://github.com/wayne-stewart/wayne-stewart.github.io.git
+
+git clone https://github.com/wayne-stewart/cs_nexus.git
+
+git clone https://github.com/wayne-stewart/FileX.git
+
+git clone https://github.com/wayne-stewart/stb.git
+
+git clone https://github.com/wayne-stewart/js-tetris.git
+
+git clone https://github.com/wayne-stewart/js-visual-sort.git
+
+git clone https://github.com/wayne-stewart/js-fractal.git
+
+git clone https://github.com/wayne-stewart/learn_rust.git
+
+git clone https://github.com/wayne-stewart/project-euler.git
+
+git clone https://github.com/wayne-stewart/Blind-Turtle.git
+
+git clone https://github.com/wayne-stewart/js-arcade.git
+
+git clone https://github.com/wayne-stewart/js-mk.git
+
